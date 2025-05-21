f38ddf08a4024a3184c54ba30388d368ecd595b5
Olesya Novoseltseva <olesya.n2003@gmail.com>
2025-05-21 14:27:44 +0300

    Upadate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//import './pages/index.css';
+import './pages/index.css';
 import { createCard } from './scripts/card.js';
 import { openModal, closeModal } from './scripts/modal.js';
 //import { validateCardForm, validateProfile } from './validation.js';
